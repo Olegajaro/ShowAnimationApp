859e45a316e628fc00aa2c47b6b0ed8230f3b859
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-09-21 20:27:09 +0300

    Setup project

---
+++ ShowAnimationApp/Models/SpringAnimation.swift
@@ -0,0 +1,8 @@
+//
+//  SpringAnimation.swift
+//  ShowAnimationApp
+//
+//  Created by Олег Федоров on 21.09.2021.
+//
+
+import Foundation
--- ShowAnimationApp/ViewControllers/MainViewController.swift
+++ ShowAnimationApp/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ShowAnimationApp
 //
 //  Created by Олег Федоров on 21.09.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
