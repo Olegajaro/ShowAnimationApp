f6b27144ee158a86680657698298c41b7ff46023
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-09-21 23:29:32 +0300

    Second Version App

--- ShowAnimationApp/ViewControllers/MainViewController.swift
+++ ShowAnimationApp/ViewControllers/MainViewController.swift
@@ -10,6 +10,7 @@ import Spring
 class MainViewController: UIViewController {
 
     @IBOutlet var animationView: SpringView!
+    @IBOutlet var runAnimationButton: SpringButton!
     
     @IBOutlet var presetLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
@@ -20,16 +21,33 @@ class MainViewController: UIViewController {
     private var animations = Animation.getAnimations()
     
     override func viewDidLoad() {
-        
+        presetLabel.text = "Preset: \(animationView.animation)"
+        curveLabel.text = "Curve: \(animationView.curve)"
+        forceLabel.text = "Force: \(animationView.force)"
+        durationLabel.text = "Duration: \(animationView.duration)"
+        delayLabel.text = "Delay: \(animationView.delay)"
     }
     
     @IBAction func runAnimationButtonPressed(_ sender: SpringButton) {
+        
         animationView.animation = animations.randomElement()?.preset ?? ""
         animationView.curve = animations.randomElement()?.curve ?? ""
         animationView.force = cgFloat(forces: animations).randomElement() ?? 0
         animationView.duration = cgFloat(durations: animations).randomElement() ?? 0
         animationView.delay = cgFloat(delays: animations).randomElement() ?? 0
         animationView.animate()
+        
+        
+        presetLabel.text = "Preset: \(animationView.animation)"
+        curveLabel.text = "Curve: \(animationView.curve)"
+        forceLabel.text = "Force: \(string(from: animationView.force))"
+        durationLabel.text = "Duration: \(string(from: animationView.duration))"
+        delayLabel.text = "Delay: \(string(from: animationView.delay))"
+        
+        runAnimationButton.setTitle(
+            "Run \(animationView.animation)",
+            for: .normal
+        )
     }
     
     private func cgFloat(forces array: [Animation]) -> [CGFloat] {
@@ -42,6 +60,10 @@ class MainViewController: UIViewController {
     
     private func cgFloat(delays array: [Animation]) -> [CGFloat] {
         array.map { CGFloat($0.delay) }
+    }
+    
+    private func string(from animationParameter: CGFloat) -> String {
+        String(format: "%.2f", animationParameter)
     }
 }
 
